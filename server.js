937052a5a08ea845e9b3bf07d6cef88b93e7f0a3
mikhail-na <115450336+mikhail-na@users.noreply.github.com>
2023-08-11 09:06:25 +0200

    commit

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://MykhailoNaz:di2uUUre0lwTTwei@cluster0.prplc6k.mongodb.net/contacts-db?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST).then(() => {
+  app.listen(3000);
+  console.log("Database connection successful");
+}).catch(err => {
+  console.log(err.message);
+  process.exit(1);
 })
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
